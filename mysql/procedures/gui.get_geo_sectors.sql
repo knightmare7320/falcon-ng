d33e26f8156001ddfdf138642ac1a325929c1e43
Brian Knight <brian.k.knight@gmail.com>
2023-11-02 17:03:52 -0500

    use spatial queries

--- mysql/procedures/gui.get_geo_sectors.sql
+++ mysql/procedures/gui.get_geo_sectors.sql
@@ -1,7 +1,3 @@
-DROP PROCEDURE IF EXISTS gui.get_geo_sectors;
-
-DELIMITER $$
-$$
 CREATE PROCEDURE gui.get_geo_sectors(
    IN in_MIN_LATITUDE  FLOAT,
    IN in_MAX_LATITUDE  FLOAT,
@@ -9,21 +5,24 @@ CREATE PROCEDURE gui.get_geo_sectors(
    IN in_MAX_LONGITUDE FLOAT
 )
 BEGIN
+   DECLARE v_geo_boundary POLYGON;
+   SET v_geo_boundary = st_srid(polygon(linestring(
+                           point(in_MIN_LONGITUDE,in_MAX_LATITUDE), 
+                           point(in_MIN_LONGITUDE,in_MIN_LATITUDE), 
+                           point(in_MAX_LONGITUDE,in_MIN_LATITUDE), 
+                           point(in_MAX_LONGITUDE,in_MAX_LATITUDE), 
+                           point(in_MIN_LONGITUDE,in_MAX_LATITUDE)
+                        )),4326);
+                     
    SELECT   s.cascade_code, 
             s.latitude,
-	        s.longitude,
-	        se.sector_number,
-	        se.azimuth,
-	        a.horizontal_bw
+	         s.longitude,
+	         se.sector_number,
+	         se.azimuth,
+	         a.horizontal_bw
    FROM     locations.sites s 
             JOIN equipment.sectors se ON s.cascade_code = se.cascade_code 
             JOIN equipment.antennas a ON a.antenna_id = se.antenna_id         
-   WHERE    s.latitude  >= in_MIN_LATITUDE  AND
-            s.latitude  <  in_MAX_LATITUDE  AND
-            s.longitude >= in_MIN_LONGITUDE AND
-            s.longitude <  in_MAX_LONGITUDE
-   ORDER BY s.cascade_code,
-            se.sector_number;
+   WHERE    st_contains(v_geo_boundary, s.geo_point)   
+   ORDER BY s.cascade_code;
 END
-$$
-DELIMITER ;
--- mysql/procedures/gui.get_geo_sites.sql
+++ mysql/procedures/gui.get_geo_sites.sql
@@ -1,7 +1,3 @@
-DROP PROCEDURE IF EXISTS gui.get_geo_sites;
-
-DELIMITER $$
-$$
 CREATE PROCEDURE gui.get_geo_sites(
    IN in_MIN_LATITUDE  FLOAT,
    IN in_MAX_LATITUDE  FLOAT,
@@ -9,15 +5,19 @@ CREATE PROCEDURE gui.get_geo_sites(
    IN in_MAX_LONGITUDE FLOAT
 )
 BEGIN
+   DECLARE v_geo_boundary POLYGON;
+   SET v_geo_boundary = st_srid(polygon(linestring(
+                           point(in_MIN_LONGITUDE,in_MAX_LATITUDE), 
+                           point(in_MIN_LONGITUDE,in_MIN_LATITUDE), 
+                           point(in_MAX_LONGITUDE,in_MIN_LATITUDE), 
+                           point(in_MAX_LONGITUDE,in_MAX_LATITUDE), 
+                           point(in_MIN_LONGITUDE,in_MAX_LATITUDE)
+                        )),4326);
+
 	SELECT   s.cascade_code, 
             s.latitude,
 	         s.longitude
    FROM     locations.sites s
-   WHERE    s.latitude  >= in_MIN_LATITUDE  AND
-            s.latitude  <  in_MAX_LATITUDE  AND
-            s.longitude >= in_MIN_LONGITUDE AND
-            s.longitude <  in_MAX_LONGITUDE
+   WHERE    st_contains(v_geo_boundary, geo_point) -- st_srid(point(longitude,latitude),4326)
    ORDER BY s.cascade_code;
 END
-$$
-DELIMITER ;
