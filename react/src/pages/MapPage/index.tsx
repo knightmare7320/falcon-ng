90ffcf3c6dfef10c1d9ab1105e39c1333588a25e
Brian Knight <brian.k.knight@gmail.com>
2024-01-06 15:46:44 -0600

    basic maps working

--- react/src/pages/MapPage/index.tsx
+++ react/src/pages/MapPage/index.tsx
@@ -1,16 +1,16 @@
 import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css"
 
-import "./MapPage.module.css";
+import styles from "./MapPage.module.css";
 
 export default function MapPage() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer className={ styles['leaflet-container'] } center={[41.993250, -87.800073]} zoom={17} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[41.993250, -87.800073]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
