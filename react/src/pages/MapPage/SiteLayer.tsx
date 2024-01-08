a9165c045d40d4c70f79851bf54791ec54eaf3f0
Brian Knight <brian.k.knight@gmail.com>
2024-01-08 15:46:46 -0600

    siteslayer

--- react/src/pages/MapPage/SiteLayer.tsx
+++ react/src/pages/MapPage/SiteLayer.tsx
@@ -1,56 +1,59 @@
 import L, { Coords, DoneCallback, GridLayer} from "leaflet";
 import { createLayerComponent  } from "@react-leaflet/core";
 
-
 class SiteLayer extends GridLayer {
+  markerStore = {};
 
-  initialize(url, options) {
-    console.log('INITIALIZE');
-    this._url = url;
-    this._markerStore = {};
-    L.GridLayer.prototype.initialize.call(this, options)
+  constructor() {
+    super();
   }
-
 
   createTile(coords: Coords, done: DoneCallback) {
     var key = this._tileCoordsToKey(coords);
 
     var tile = document.createElement('div');
-    tile.innerHTML = key;
-    tile.style.outline = '1px dotted gray';
-    tile.style.color = 'black';
-    tile.style.fontWeight = 'bold';
+    // tile.innerHTML = key;
+    // tile.style.outline = '1px dotted gray';
+    // tile.style.color = 'black';
+    // tile.style.fontWeight = 'bold';
     // tile.style.background = 'rgba(0,0,0,0.1)';
     
-    fetch(`http://localhost:3000/api/geo/sites/${coords.z}/${coords.x}/${coords.y}`)
-      .then(response => response.json())
-      .then(json => {
-        if(json.rows.length > 0) {
-          console.log(json)
-        }
-        var markers = json.rows.map(function(site){
-          const object = L.circleMarker(
-            [site.latitude, site.longitude],
-            {radius: 4, weight: 1, color: 'white', fillOpacity: 1.0, fillColor: 'green'/*, title: site.cascade_code*/,
-          });
-          // object.on('click', () => this.router.navigate(['site', site.cascade_code]));
-          object.bindTooltip(site.cascade_code).openTooltip();
-          return object;
-        });
-        var group = L.layerGroup(markers);
-        this._markerStore[key] = group;
-        group.addTo(this._map);
+    if (coords.z >= 14) {
+      fetch(`http://localhost:3000/api/geo/sites/${coords.z}/${coords.x}/${coords.y}`)
+        .then(response => response.json())
+        .then(json => {
+          if (json.rows.length > 0) {
+            var markers = json.rows.map(site => {
+              const object = L.circleMarker(
+                [site.latitude, site.longitude],
+                {radius: 4, weight: 1, color: 'white', fillOpacity: 1.0, fillColor: 'green'/*, title: site.cascade_code*/,
+              });
+              // object.on('click', () => this.router.navigate(['site', site.cascade_code]));
+              object.bindTooltip(site.cascade_code + '|' + key).openTooltip();
+              return object;
+            });
+            var group = L.layerGroup(markers);
+            this.markerStore[key] = group;
+            group.addTo(this._map);
 
+            console.log('add', key, Object.keys(this.markerStore).length, Object.keys(this.markerStore).join('|'));
+            done();
+          }
+        });
+      } else {
         done();
-      });
+      }
    
     return tile;
   }
 
-  
+
   _removeTile(key: string) {
-    console.log('remove', key);
-    delete this._markerStore[key];
+    if (this.markerStore[key]) {
+      this.markerStore[key].clearLayers();
+      delete this.markerStore[key];
+    }
+    console.log('REMOVE', key, Object.keys(this.markerStore).join('|'));
 
     return L.GridLayer.prototype._removeTile.call(this, key);
   }
