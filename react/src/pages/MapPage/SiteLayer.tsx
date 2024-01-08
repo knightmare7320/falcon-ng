d283b8eb2f9aecccbed301f0fec768e95e8443b2
Brian Knight <brian.k.knight@gmail.com>
2024-01-08 10:20:19 -0600

    kitten layer

--- react/src/pages/MapPage/SiteLayer.tsx
+++ react/src/pages/MapPage/SiteLayer.tsx
@@ -1,13 +1,48 @@
-import { useMapEvents } from "react-leaflet";
+import L, { Coords, DoneCallback, GridLayer, imageOverlay} from "leaflet";
+import { createLayerComponent, LayerProps  } from "@react-leaflet/core";
+import { ReactNode } from "react";
 
-export default function SiteLayer() {
-  const map = useMapEvents({
-    click: () => {
-      map.locate()
-    },
-    locationfound: (location) => {
-      console.log('location found:', location)
-    },
-  })
-  return null
+interface KittenProps extends LayerProps {
+  userId: string,
+  children?: ReactNode // PropsWithChildren is not exported by @react-leaflet/core
 }
+
+class Kitten extends L.TileLayer {
+  getTileUrl(coords: L.Coords) {
+    var i = Math.ceil( Math.random() * 4 );
+    return "https://placekitten.com/256/256?image=" + i;
+  }
+
+  getAttribution() {
+    return "<a href='https://placekitten.com/attribution.html'>PlaceKitten</a>"
+  }
+}
+
+const createKittenLayer = (props: KittenProps, context:any) => {
+  const instance = new Kitten("placeholder", {...props});
+  return {instance, context};
+}
+
+const updateKittenLayer = (instance: any, props: KittenProps, prevProps: KittenProps) => {
+  if (prevProps.userId !== props.userId) {
+    if (instance.setUserId) instance.setUserId(props.userId)
+  }
+}
+
+const KittenLayer = createLayerComponent(createKittenLayer, updateKittenLayer);
+export default KittenLayer;
+
+
+// import { useMapEvents } from "react-leaflet";
+
+// export default function SiteLayer() {
+//   const map = useMapEvents({
+//     click: () => {
+//       map.locate()
+//     },
+//     locationfound: (location) => {
+//       console.log('location found:', location)
+//     },
+//   })
+//   return null
+// }
--- react/src/pages/MapPage/index.tsx
+++ react/src/pages/MapPage/index.tsx
@@ -1,12 +1,35 @@
-import { MapContainer, TileLayer, Popup, Marker, useMapEvents } from "react-leaflet";
+import { useEffect } from "react";
+import { useMap, MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css"
 
 import styles from "./MapPage.module.css";
 import SiteLayer from "./SiteLayer";
 
+// https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=<YOUR_API_KEY>&units=metric
+
+function MyMap() {
+  const map = useMap();
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success, error);
+  });
+
+  function success(position: GeolocationPosition) {
+    map.panTo({lat: position.coords.latitude, lng: position.coords.longitude})
+  }
+
+  function error() {
+    console.log("Unable to retrieve location");
+  }
+
+  return null;
+}
+
+
 export default function MapPage() {
   return (
-    <MapContainer className={ styles['leaflet-container'] } center={[41.993250, -87.800073]} zoom={17} scrollWheelZoom={false}>
+    <MapContainer className={ styles['leaflet-container'] } center={[41.993250, -87.800073]} zoom={17} scrollWheelZoom={true}>
+      <MyMap />
+
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -18,7 +41,6 @@ export default function MapPage() {
       </Marker>
 
       <SiteLayer />
-      
     </MapContainer>
   ); 
 }
