e5a33ec1192519b88c5ba303e3648f73cc5daa1c
Brian Knight <brian.k.knight@gmail.com>
2024-01-20 13:14:28 -0600

    starting pictures container

--- react/src/components/ui/Breadcrumbs.tsx
+++ react/src/components/ui/Breadcrumbs.tsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faCaretRight } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./Breadcrumbs.module.css";
 
@@ -19,13 +19,13 @@ export default function Breadcrumbs({region_id, l4_market_id, l5_market_id, clus
   let content = [];
   
   if (cascade_code) {
-    content.unshift(<>&gt; {cascade_code}</>);
+    content.unshift(<><FontAwesomeIcon icon={faCaretRight} /> {cascade_code}</>);
   }
   
   if (cluster_id) {
     const item = uiState.clusters.find(cluster => cluster_id === cluster.id);
     if (item) {
-      content.unshift(<> &gt; <Link to={`/browse/cluster/${cluster_id}`}>{item.name}</Link></>);
+      content.unshift(<> <FontAwesomeIcon icon={faCaretRight} /> <Link to={`/browse/cluster/${cluster_id}`}>{item.name}</Link></>);
       l5_market_id = l5_market_id || item.parent_id;
     }
   }
@@ -33,7 +33,7 @@ export default function Breadcrumbs({region_id, l4_market_id, l5_market_id, clus
   if (l5_market_id) {
     const item = uiState.l5_markets.find(l5_market => l5_market_id === l5_market.id);
     if (item) {
-      content.unshift(<> &gt; <Link to={`/browse/l5_market/${l5_market_id}`}>{item.name}</Link></>);
+      content.unshift(<> <FontAwesomeIcon icon={faCaretRight} /> <Link to={`/browse/l5_market/${l5_market_id}`}>{item.name}</Link></>);
       l4_market_id = l4_market_id || item.parent_id;
     }
   }
@@ -41,7 +41,7 @@ export default function Breadcrumbs({region_id, l4_market_id, l5_market_id, clus
   if (l4_market_id) {
     const item = uiState.l4_markets.find(l4_market => l4_market_id === l4_market.id);
     if (item) {
-      content.unshift(<> &gt; <Link to={`/browse/l4_market/${l4_market_id}`}>{item.name}</Link></>);
+      content.unshift(<> <FontAwesomeIcon icon={faCaretRight} /> <Link to={`/browse/l4_market/${l4_market_id}`}>{item.name}</Link></>);
       region_id = region_id || item.parent_id;
     }
   }
@@ -49,7 +49,7 @@ export default function Breadcrumbs({region_id, l4_market_id, l5_market_id, clus
   if (region_id) {
     const item = uiState.regions.find(region => region_id === region.id);
     if (item) {
-      content.unshift(<> &gt; <Link to={`/browse/region/${region_id}`}>{item.name}</Link></>);
+      content.unshift(<> <FontAwesomeIcon icon={faCaretRight} /> <Link to={`/browse/region/${region_id}`}>{item.name}</Link></>);
     }
   }
   
