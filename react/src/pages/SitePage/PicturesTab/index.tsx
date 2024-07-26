1bbd439c07a2babc64604c0c67e3681c3bf08001
Brian Knight <brian.k.knight@gmail.com>
2024-07-26 08:50:12 -0500

    "fix" mysql for v9

--- react/src/pages/SitePage/PicturesTab/index.tsx
+++ react/src/pages/SitePage/PicturesTab/index.tsx
@@ -7,16 +7,16 @@ function getText(cascadeCode:string, picture:string) {
 
 export default function PicturesTab({cascadeCode, pictures}:{cascadeCode:string, pictures:string[]}) {
   return <>
-  { pictures.map(
-    (picture, index) =>
-      <div className="responsive">
-        <div className="gallery">
-          <img src={`/pictures/image/${cascadeCode}/${picture}?width=800&height=800" alt="Site Picture`} width="600" height="400" key={index} />
-          <div className="desc">{getText(cascadeCode, picture)}</div>
+    { pictures.map(
+      (picture, index) =>
+        <div className="responsive">
+          <div className="gallery">
+            <img src={`/pictures/image/${cascadeCode}/${picture}?width=800&height=800" alt="Site Picture`} width="600" height="400" key={index} />
+            <div className="desc">{getText(cascadeCode, picture)}</div>
+          </div>
         </div>
-      </div>
-  )}
+    )}
 
-  <div className="clearfix"></div>
+    <div className="clearfix"></div>
   </>;
 }
